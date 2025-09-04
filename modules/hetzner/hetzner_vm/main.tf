a5f296bb6576fefca41c3fb9f0e1186073aa8487
Ahmed Said <ahmed.el.sayeed1914@gmail.com>
2025-09-04 20:12:51 +0300

    remove undeeded rules

--- modules/hetzner/hetzner_vm/main.tf
+++ modules/hetzner/hetzner_vm/main.tf
@@ -1,40 +1,4 @@
 locals {
-  ## Block access to everything 
-  base_firewall_rules = [
-    {
-      description = "block all tcp traffic"
-      direction   = "in"
-      protocol    = "tcp"
-      source_ips  = ["0.0.0.0/0", "::/0"]
-      port        = "1-65535"
-      action      = "drop"
-    },
-    {
-      description = "block all udp traffic"
-      direction   = "in"
-      protocol    = "udp"
-      source_ips  = ["0.0.0.0/0", "::/0"]
-      port        = "1-65535"
-      action      = "drop"
-    },
-    {
-      description = "block all icmp traffic"
-      direction   = "in"
-      protocol    = "icmp"
-      source_ips  = ["0.0.0.0/0", "::/0"]
-      action      = "drop"
-    }
-  ]
-  # create a new firewall list based on base_firewall_rules but with direction-protocol-port as key
-  # this is needed to avoid duplicate rules
-  firewall_rules = {
-    for rule in local.base_firewall_rules :
-    format("%s-%s-%s",
-      lookup(rule, "direction", "null"),
-      lookup(rule, "protocol", "null"),
-      lookup(rule, "port", "null")
-    ) => rule
-  }
 
   # do the same for var.extra_firewall_rules
   extra_firewall_rules = {
@@ -45,18 +9,12 @@ locals {
       lookup(rule, "port", "null")
     ) => rule
   }
-
-  # merge the two lists
-  firewall_rules_merged = merge(local.firewall_rules, local.extra_firewall_rules)
-
-  # convert the merged list back to a list
-  firewall_rules_list = values(local.firewall_rules_merged)
 }
 
 resource "hcloud_firewall" "this" {
   name = "${var.vm_name}-firewall"
   dynamic "rule" {
-    for_each = local.firewall_rules_list
+    for_each = local.extra_firewall_rules
     content {
       description     = coalesce(rule.value.description,"Firewall Rule")
       direction       = rule.value.direction
